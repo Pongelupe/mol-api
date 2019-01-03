cf013ca5137c91053e191f7f86ba7c59eb909cb9
Pongelupe <pongelupe1998@gmail.com>
2019-01-02 23:37:34 -0200

    estrutura inicial do projeto

---
+++ src/main/java/br/com/mol/molapi/MolapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mol.molapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolapiApplication.class, args);
+	}
+}
+
---
+++ src/test/java/br/com/mol/molapi/MolapiApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.mol.molapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MolapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
