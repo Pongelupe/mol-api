7d90fe0dec8afd95fdf9e24540288e437f2e99f8
Pongelupe <pongelupe1998@gmail.com>
2019-02-09 08:24:54 -0200

    heroku files

--- src/main/java/br/com/mol/molapi/configurations/CustomConfiguration.java
+++ src/main/java/br/com/mol/molapi/configurations/CustomConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.mol.molapi.configurations;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,12 +12,17 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import br.com.mol.molapi.entity.Prescription;
 import br.com.mol.molapi.entity.serializers.PrescriptionSerializer;
 
 @Configuration
 public class CustomConfiguration {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -23,13 +31,18 @@ public class CustomConfiguration {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper()
-				.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 				.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-		
-		mapper.registerModule(new SimpleModule()
-				.addSerializer(Prescription.class, new PrescriptionSerializer()));
-		
+
+		mapper.registerModule(new SimpleModule().addSerializer(Prescription.class, new PrescriptionSerializer()));
+
 		return mapper;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
 	}
 }
